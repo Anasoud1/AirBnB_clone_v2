631afc82f8fc6212b9e974c3c4bdf4db1d95a93c
Anasoud1 <anasoudsassi@gmail.com>
2024-01-10 18:35:53 +0100

    updating 0-setup_wab_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static.
+# Sets up a web server for deployment of web_static.
 
-#sudo apt-get update
-#sudo apt-get install -y nginx
+apt-get update
+apt-get install -y nginx
 
-mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
-string_replacement="location \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tindex index.html index.htm;\n\t}\n\tservice_name _;"
-sudo sed -i "s/service_name _;/$string_replacement/" /etc/nginx/sites-enabled/default
 
-sudo service nginx restart
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $hostname;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://github.com/Anasoud1;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
