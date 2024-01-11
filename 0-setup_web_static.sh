dcedd2c42e0bb852cc90a46ee56021fcc25f0672
Anasoud1 <anasoudsassi@gmail.com>
2024-01-11 14:34:08 +0100

    updating 0-setup_web_static.sh file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
-sudo apt-get update
-sudo apt-get install -y nginx
+apt-get update
+apt-get install -y nginx
 
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo hown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
 sudo printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
+    add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
     index  index.html index.htm;
     location /hbnb_static {
@@ -31,5 +31,5 @@ sudo printf %s "server {
       internal;
     }
 }" > /etc/nginx/sites-available/default
-
+sudo nginx -t
 sudo service nginx restart
