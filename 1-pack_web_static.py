f6d40c28c431c8d82fa810c0b0baf2917ef89cb3
Anasoud1 <anasoudsassi@gmail.com>
2024-01-27 16:24:24 +0100

    adding 1-pack_web_static.py file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """A module for Fabric script that generates a .tgz archive."""
+'''
 import os
 from datetime import datetime
-from fabric.api import local, runs_once
+from invoke import task
 
-
-@runs_once
-def do_pack():
+@task
+def do_pack(c):
     """Archives the static files."""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
@@ -21,9 +21,30 @@ def do_pack():
     )
     try:
         print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
+        c.run("tar -cvzf {} web_static".format(output)
         size = os.stat(output).st_size
         print("web_static packed: {} -> {} Bytes".format(output, size))
     except Exception:
         output = None
     return output
+'''
+import os
+from datetime import datetime
+from invoke import task
+
+@task
+def do_pack(c):
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = f"versions/web_static_{d_time.year}{d_time.month}{d_time.day}{d_time.hour}{d_time.minute}{d_time.second}.tgz"
+    try:
+        print(f"Packing web_static to {output}")
+        c.run(f"tar -cvzf {output} web_static")
+        size = os.stat(output).st_size
+        print(f"web_static packed: {output} -> {size} Bytes")
+    except Exception:
+        output = None
+    return output
+
