8e1e0191907563f3de66670808910fdbf4d9ae89
Anasoud1 <anasoudsassi@gmail.com>
2024-01-27 20:25:28 +0000

    updating 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -25,10 +25,9 @@ def display_python_text(text="is_cool"):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def display_numbert(n):
-    if isinstance(int(n), int):
-    	return "{} is a number".format(n)
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
