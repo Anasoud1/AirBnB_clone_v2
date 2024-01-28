86024e1428ea0d27272de3ca06266225dd51a1c1
Anasoud1 <anasoudsassi@gmail.com>
2024-01-28 14:51:23 +0100

    updating models/engine/file_storage.py, models/engine/db_storage.py, models/state.py files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -87,3 +87,8 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """call remove() method"""
+        self.__session.remove()
+
