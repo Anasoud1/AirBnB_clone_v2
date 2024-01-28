29bc95e18711056515ae1133d62865265e2aebdf
Anasoud1 <anasoudsassi@gmail.com>
2024-01-28 17:09:41 +0100

    importing storage in state.py file

--- models/state.py
+++ models/state.py
@@ -2,7 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base, Column, String
 from sqlalchemy.orm import relationship
-from models import storage_type
+from models import storage_type, storage
 
 
 class State(BaseModel, Base):
